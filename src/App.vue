fc4fba28b31d7e5a208fcdb2af1100e18d6ba4e0
Monil Soni <monisoni@amazon.com>
2020-07-15 18:33:27 +0530

    Created Todo files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <HelloWorld msg="Hello, Monil."/>
+        <Todos v-bind:todos="todos"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from "./components/HelloWorld";
+    import Todos from './components/Todos'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Todos,
+            HelloWorld
+        },
+        data() {
+            return {
+                todos: [
+                    {id: 1, title: "Todo 1", completed: false},
+                    {id: 2, title: "Todo 2", completed: true},
+                    {id: 3, title: "Todo 3", completed: true}
+                ]
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="todo-item">
+        <p>
+            {{ todo.title }}
+        </p>
+    </div>
+</template>
+
+<script>
+   export default {
+       name: "TodoItem",
+       props: ["todo"]
+   }
+</script>
+
+<style scoped>
+    .todo-item {
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+    }
+</style>
